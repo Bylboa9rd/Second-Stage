5f54b91db288566d294b0d1a3bbf1c51f402e69f
Bylboa9rd <110336767+Bylboa9rd@users.noreply.github.com>
2022-11-27 17:11:17 +0100

    Update Program.cs

--- Task Three/Task Three/Program.cs
+++ Task Three/Task Three/Program.cs
@@ -1,33 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Task Three: Write a program that translates a text to Pig Latin and back. English is translated
-//to Pig Latin by taking the first letter of every word, moving it to the end of the word and adding 'ay'.
-//such that "The quick brown fox" becomes "Hetay uickqay rownbay oxfay".
-
-
-/* The user input is collected after which the sentence would be split by the spaces between the words.
- * The individual words would be stored in an array for reference after which each word loses its first letter.
- *  That (first) letter is then stored in a variable for reattachment at the end of the word. 
- *  At the end of each word, "ay" is attached, followed by the space we removed at the beginning, which
- *  would help us form the sentence in Pig Latin. Since Console.WriteLine() prints each new word on a new line,
- *  the alternative Console.Write() is adopted instead.
- */
-
-Console.WriteLine("Write your favorite quote and we will translate it to Pig Latin for you: ");
-string sentence = Console.ReadLine();
-string[] words = sentence.Split(' ');
-
-foreach (string word in words)
+using System;
+namespace Translator
 {
-    string toAppend = word.Substring(0, 1); 
-    string newWord = word.Remove(0, 1);
+    class PigLatinTranslator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your favorite quote and we will translate it to Pig Latin for you: ");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(' ');
 
-    newWord += toAppend;
+            foreach (string word in words)
+            {
+                string toAppend = word.Substring(0, 1); 
+                string newWord = word.Remove(0, 1);
 
-    newWord = newWord.Insert(newWord.Length, "ay");
+                newWord += toAppend;
 
-    newWord += " ";
+                newWord = newWord.Insert(newWord.Length, "ay");
 
-    Console.Write(newWord);
+                newWord += " ";
+
+                Console.Write(newWord);
+            }
+        }
+        
+    }
+    
 }
 
 
